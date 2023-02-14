8b8effb514b3b9f326a7c41ac2e635c12c564007
danwill <daniel.willett2@gmail.com>
2023-02-13 17:29:21 -0800

    client side search parameters to get yelp restaurants

--- controllers/api/yelp.js
+++ controllers/api/yelp.js
@@ -11,31 +11,40 @@ const router = require('express').Router();
 //     "cuisine": ,
 // }
 
-router.get('/restaurantSearch', async (req, res) => {
+router.get('/search', async (req, res) => {
     try {
 
-    let params = [
-        { location: req.body.location, 
-        term: req.body.cuisine, 
-        // attributes: 
-    }];
+      console.log(req.query)
+      let cats = req.query.categories.split("_")
+      console.log(cats)
 
-    const searchResults = await yelp.query('businesses/search', params)
-    let searchObj = JSON.parse(searchResults)
-    let businesses = searchObj.businesses;
-    const filterBusinesses = businesses.filter((business) => {
-      let categories = business.categories
-      console.log(categories)
-      for (let i=0; i<categories.length; i++) {
-        if (categories[i].alias.toLowerCase() == params[0].term.toLowerCase()) {
-          return business
+      let location = `location=${req.query.city}`;
+    let categories = [];
+    for (let i = 0; i < cats.length; i++) {
+      categories.push(`categories=${cats[i]}`)  
+    }
+    console.log(categories)
+    categories = categories.join("&")
+    console.log(categories)
+    let filters = `${location}&${categories}`;
+    console.log(filters)
+    // const searchResults = await yelp.query('businesses/search', params)
+
+    const searchResults = await fetch(`https://api.yelp.com/v3/businesses/search?${filters}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey
         }
-      }
-      
     })
-    console.log(filterBusinesses)
+    // console.log(searchResults.json())
+    const searchObj = await searchResults.json()
+    console.log(searchObj)
+    res.json(searchObj)
+    let businesses = searchObj.businesses;
+    console.log(businesses)
 
-    res.status(200).send(filterBusinesses)
+    // res.status(200).send(filterBusinesses)
  
     } catch (err) {
         console.log(err)
@@ -56,15 +65,15 @@ router.get('/categories', async (req, res) => {
 
     const js = await data.json();
     categories = js.categories;
-    foodCatsObj = categories.filter((cat) => {
-        
+    foodCatsObj = categories.filter((cat) => {     
         if (cat.alias == "food" || cat.alias == "restaurants" || cat.parent_aliases.includes("restaurants")) {
             return cat
         }
     })
-    foodCatsArray =  foodCatsObj.map((obj) => obj.title);
-    console.log(foodCatsArray)
-    res.status(200).json(foodCatsArray)
+    console.log(foodCatsObj)
+    // foodCatsArray =  foodCatsObj.map((obj) => obj.title);
+    // console.log(foodCatsArray)
+    res.status(200).json(foodCatsObj)
 
 } catch (err) {
     res.status(500).json(err)
--- public/js/searchRestaurants.js
+++ public/js/searchRestaurants.js
@@ -1,20 +1,29 @@
 // Get food categories list
-const searchBtnEl = document.getElementById("search");
+const searchBtnEl = document.getElementById("searchBtn");
+const searchFieldEl = document.getElementById("search");
 const categoryListEl = document.getElementById("show-list");
 const filtersEl = document.getElementById("filters");
 
-const searchRestaurants = async () => {
+const getFilters = (event) => {
+    let currentFilters = [...filtersEl.children];
+    let filterIds = currentFilters.map((obj) => obj.id)
+    return filterIds
+}
+
+const addFilters = async () => {
   const categoriesObj = await fetch("/api/yelp/categories");
   const categories = await categoriesObj.json();
   console.log(categories);
 
-  searchBtnEl.addEventListener("keyup", async (event) => {
+  searchFieldEl.addEventListener("keyup", async (event) => {
     event.preventDefault();
     var searchText = event.target.value;
     if (!searchText == "") {
       let regexp = new RegExp(searchText, "i");
-      let matchList = categories.filter((x) => regexp.test(x));
+      let matchList = categories.filter((x) => regexp.test(x.title));
       console.log("filling in");
+      console.log(matchList)
+      let categoryTitles = matchList.map((cat) => cat.title);
 
       // if existing list items no longer match, delete them
       let listItems = categoryListEl.children;
@@ -22,7 +31,7 @@ const searchRestaurants = async () => {
       for (let i = 0; i < listItems.length; i++) {
         let listId = listItems[i].id;
         console.log(listId);
-        if (!matchList.includes(listId)) {
+        if (!categoryTitles.includes(listId)) {
             listItems[i].remove()
         }
       }
@@ -31,15 +40,16 @@ const searchRestaurants = async () => {
       if (searchText.length >= 3) {
         matchList.forEach((cat) => {
           // if there's already a list item with this cat, don't make a new one
-          if (document.getElementById(cat) == null) {
+          if (document.getElementById(cat.title) == null) {
             let catItem = document.createElement("a");
             catItem.setAttribute(
               "class",
               "list-group-item list-group-item-action border-1"
             );
             catItem.setAttribute("href", "#");
-            catItem.setAttribute("id", cat);
-            catItem.textContent = cat;
+            catItem.setAttribute("id", cat.title);
+            catItem.setAttribute("data-alias", cat.alias)
+            catItem.textContent = cat.title;
             categoryListEl.appendChild(catItem);
           }
         });
@@ -51,19 +61,53 @@ const searchRestaurants = async () => {
     categoryListEl.addEventListener("click", (event) => {
         event.preventDefault()
         event.stopPropagation()
-        let category = event.target.id
-        let currentFilters = [...filtersEl.children];
-        let filterIds = currentFilters.map((obj) => obj.id)
+        console.log(event.target)
+        let category = event.target.getAttribute("id")
+        let alias = event.target.getAttribute("data-alias")
+        console.log(alias)
+        let filters = getFilters(event);
         
-        if (!filterIds.includes(category)) {
+        console.log(filters)
+        
+        if (!filters.includes(category)) {
             let newFilter = document.createElement('li');
             newFilter.setAttribute("id", category)
             newFilter.setAttribute("class", "list-group-item")
+            newFilter.setAttribute("data", alias)
             newFilter.textContent = category;
             filtersEl.appendChild(newFilter)
         }
-    },)
+    })
+    
   });
 };
 
-searchRestaurants();
+addFilters();
+
+// find restaurants matching filters categories
+searchBtnEl.addEventListener('click', async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let currentFilters = filtersEl.children;
+    let aliases;
+    for (let i = 0; i<currentFilters.length; i++) {
+        console.log(currentFilters[i])
+        if (i == 0) {
+            aliases = currentFilters[i].getAttribute("data");
+        } else {
+            aliases = aliases.concat("_", currentFilters[i].getAttribute("data"))
+        }
+         
+    }
+     
+    console.log(aliases)
+    
+    // need a city and state query parameter
+
+    const yelpData = await fetch(`/api/yelp/search?categories=${aliases}&city=Goleta&state=CA`)
+    const yelpObj = await yelpData.json()
+    console.log(yelpObj)
+})
+
+
